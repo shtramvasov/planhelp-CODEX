eb3717c77d5c05e0dee09c0b0296fec2fcea9e26
Анатолий Предеин <predeinay@Anatolijs-MacBook-Pro.local>
2023-01-19 22:06:58 +0500

    mvp user_roles

--- server/routers/disk.js
+++ server/routers/disk.js
@@ -17,28 +17,37 @@ router.get('/:entity_id?', async (req, res, next) => {
         };
         if (entity_id) {
             rootEntity = (await mysql.query(con, 
-                `select entity_id,
-                        entity_name,
-                        entity_note,
-                        entity_type, 
-                        parent_entity_id, 
-                        created_by, 
-                        created_on
-                   from disk_entity 
-                  where entity_id = ?`, [entity_id]))[0];
+                `select de.entity_id,
+                        de.entity_name,
+                        de.entity_note,
+                        de.entity_type, 
+                        de.parent_entity_id, 
+                        de.created_by, 
+                        de.created_on
+                   from disk_entity de
+                  where de.entity_id = ?
+                    and exists (select 1 
+                                  from disk_entity_users deu 
+                                 where deu.entity_id = de.entity_id
+                                   and deu.user_id = ?)`, [entity_id, req.userModel.user_id]))[0];
         }
         if (rootEntity.entity_type !== "FILE" && !search) {
             const childEntityList = await mysql.query(con, 
-                `select entity_id,
-                        entity_name, 
-                        entity_type, 
-                        parent_entity_id, 
-                        created_by, 
-                        created_on
-                    from disk_entity, (select ? p_entity_id) params
-                    where (parent_entity_id = params.p_entity_id or (params.p_entity_id is null and parent_entity_id is null))
-                      and disk_entity.is_deleted = 'N'
-                    order by entity_type desc, entity_name, entity_id`, [entity_id]);
+                `select de.entity_id,
+                        de.entity_name, 
+                        de.entity_type, 
+                        de.parent_entity_id, 
+                        de.created_by, 
+                        de.created_on
+                   from disk_entity de, (select ? p_entity_id) params
+                  where (de.parent_entity_id = params.p_entity_id or (params.p_entity_id is null and de.parent_entity_id is null))
+                    and de.is_deleted = 'N'
+                    and exists (select 1 
+                                  from disk_entity_users deu 
+                                 where deu.entity_id = de.entity_id
+                                   and deu.user_id = ?)
+                  order by de.entity_type desc, de.entity_name, de.entity_id`, 
+                [entity_id , req.userModel.user_id]);
             rootEntity.childEntityList = childEntityList;
         }
         if (search) {
@@ -67,38 +76,39 @@ router.get('/:entity_id?', async (req, res, next) => {
     }
 });
 
-// Возвращает эелементы при поиске по словам
-router.get('/find/:search', async (req, res, next) => {
-    const { search } = req.params;
-    let con;
-    try {
-        con = await mysql.getConnection();
-        result = [];
-        if (search) {
-            result = await mysql.query(con, 
-                `select entity_id,
-                        entity_name,
-                        entity_note,
-                        entity_type, 
-                        parent_entity_id, 
-                        created_by, 
-                        created_on
-                   from disk_entity, (select ? p_search) params
-                  where upper(entity_name) like concat('%',upper(params.p_search),'%')`, [search]);
-        }
-        res.send(result);
-    } catch(error) {
-        next(error);
-    } finally {
-        con && await mysql.releaseConnection(con);
-    }
-});
+// // Возвращает эелементы при поиске по словам
+// router.get('/find/:search', async (req, res, next) => {
+//     const { search } = req.params;
+//     let con;
+//     try {
+//         con = await mysql.getConnection();
+//         result = [];
+//         if (search) {
+//             result = await mysql.query(con, 
+//                 `select entity_id,
+//                         entity_name,
+//                         entity_note,
+//                         entity_type, 
+//                         parent_entity_id, 
+//                         created_by, 
+//                         created_on
+//                    from disk_entity, (select ? p_search) params
+//                   where upper(entity_name) like concat('%',upper(params.p_search),'%')`, [search]);
+//         }
+//         res.send(result);
+//     } catch(error) {
+//         next(error);
+//     } finally {
+//         con && await mysql.releaseConnection(con);
+//     }
+// });
 
 
 // Сохраняет новую папку или новый файл
 router.post('/', async (req, res, next) => {
     let con;
     let parent;
+    let parentUsers;
     try {
         const { entity_name, entity_type, entity_note, parent_entity_id } = req.body;
         if (!entity_name || !entity_type) throw "No entity_name or entity_type in data";
@@ -108,12 +118,20 @@ router.post('/', async (req, res, next) => {
         if (parent_entity_id) {
             // блокируем запись, вдруг она кем то процессится в данный момент
             parent = (await mysql.query(con, 
-            `select entity_tree
-                from disk_entity
+              `select entity_tree
+                 from disk_entity
                 where entity_id = ?
                   for update`, [ parent_entity_id ]))[0];
+            parentUsers = await mysql.query(con, 
+              `select user_id, user_role
+                 from disk_entity_users
+                where entity_id = ?
+                  for update`, [ parent_entity_id ])
+        } else {
+            parentUsers = [{user_id : req.userModel.user_id, user_role : "OWNER"}];
         }
         const entityTree = parent_entity_id ? parent.entity_tree + '/' : "";
+        // Создаем головную запись
         await mysql.query(con, 
             `insert into disk_entity(
                 entity_name, 
@@ -136,9 +154,16 @@ router.post('/', async (req, res, next) => {
                 )`,
             [ entity_name, entity_note, entity_type, parent_entity_id, req.userModel.user_id, entityTree ] );
         const entity_id = (await mysql.query(con,`select LAST_INSERT_ID() entity_id`))[0].entity_id;
+        // присваиваем денормализованное дерево вложенности
         await mysql.query(con,
             `update disk_entity set entity_tree = concat(entity_tree, ?) where entity_id = ?`,
             [ entity_id, entity_id ]);
+        // устанавливаем права на созданный entity
+        for (const userRole of parentUsers) {
+            await mysql.query(con,
+                `insert into disk_entity_users(entity_id, user_id, user_role) values(?,?,?)`,
+                [ entity_id, userRole.user_id, userRole.user_role ]);
+        }
         res.send({entity_id : entity_id});
     } catch(err) {
         con && await mysql.rollback(con);
