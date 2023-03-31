c49006956aa4100c9a2451a82468da5c2311d4bc
predeinay <predeinay@gmail.com>
2023-03-31 23:22:29 +0500

    add proxy trust

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const commonNote = require('./routers/common_note');
 const auth = require('./auth');
 
 const app = express();
-
+app.set('trust proxy', 'loopback') 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
