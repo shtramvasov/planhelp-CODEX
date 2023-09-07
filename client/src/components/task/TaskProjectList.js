93ed7173581dada71a963cc225a9dc1f6086976a
Анатолий Предеин <predeinay@Anatolijs-MacBook-Pro.local>
2023-09-07 21:43:07 +0500

    add example list

--- client/src/components/task/TaskProjectList.js
+++ client/src/components/task/TaskProjectList.js
@@ -4,11 +4,32 @@ import { useNavigate , useSearchParams} from "react-router-dom";
 import { useParams } from 'react-router-dom';
 import React, { useState, useEffect, useRef } from 'react';
 import Breadcrumb from "../helpers/Breadcrumb";
+import { useSelector, useDispatch } from 'react-redux';
+import { getProject } from "../../network/TaskNetwork";
+import { addProject, addProjectList } from '../../reducers/Project';
 
 function TaskProjectForm(props) {
-
+    const dispatch = useDispatch();
     const navigate = useNavigate();
     const { project_id } = useParams();
+
+    const Project = useSelector((state) => state.project);
+    
+    // Первичная загрузка данных,
+    // Последующие загрзки при измененеии entity_id
+    useEffect(() => {
+        fetchProjectList();
+    },[]);
+
+    const fetchProjectList = () => {
+        getProject({project_id},(err,resp) => {
+            if (!err) {
+                dispatch(addProject(resp));
+            } else {
+                alert("Ошибка: "+err);
+            }
+        });
+    };
 
     return (
     <Container>
@@ -23,7 +44,7 @@ function TaskProjectForm(props) {
         <Breadcrumb 
             items={[
                 {url:`/task`, name: "Мои проекты"},
-                {url:`/task/project/add`, name: "Список задач"}
+                {url:`/task/project/add`, name: Project.project.project_name}
             ]}
         />
         </Col>
@@ -37,7 +58,7 @@ function TaskProjectForm(props) {
     </Row>
     <Row style={{marginTop: "8px"}}>
         <Col>
-            <h2>Задачи проекта</h2>
+            <h2>{Project.project.project_name}</h2>
         </Col>
     </Row>
     <Row style={{marginTop: "8px"}}>
@@ -48,11 +69,15 @@ function TaskProjectForm(props) {
             onClick={(e) => {}} 
             variant="">
                 <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">Conveying meaning to assistive technologies Android / ios</h5>
+                    <h5 class="mb-1">JavaScript behavior WEB / ЛК УК</h5>
                     <small>3 дня назад</small>
                 </div>
                 
-                <p class="mb-1">forson</p>
+                <p class="mb-1">
+                    <i className="bi bi-person"></i> forson &nbsp;
+                    <i className="bi bi-person-check"></i> predeinay &nbsp;
+                    <i className="bi bi-arrow-right"></i> timofey &nbsp;
+                </p>
                 <small><Badge bg="success">New</Badge></small>
                 
         </ListGroup.Item>
@@ -61,22 +86,32 @@ function TaskProjectForm(props) {
             onClick={(e) => {}} 
             variant="">
                 <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">Сделать то-то</h5>
+                    <h5 class="mb-1">Conveying meaning to assistive technologies Android / ios</h5>
                     <small>3 дня назад</small>
                 </div>
-                <p class="mb-1">forson</p>
-                <small>predeinay</small>
+                
+                <p class="mb-1">
+                    <i className="bi bi-person"></i> forson &nbsp;
+                    <i className="bi bi-person-check"></i> predeinay &nbsp;
+                    <i className="bi bi-arrow-right"></i> timofey &nbsp;
+                </p>
+                <small><Badge bg="warning">Testing</Badge></small>
         </ListGroup.Item>
         <ListGroup.Item key={1} 
             action href={"https://ya.ru"}
             onClick={(e) => {}} 
-            variant="dark">
+            variant="secondary">
                 <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">List group item heading</h5>
-      <small>3 days ago</small>
-    </div>
-    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-    <small>Donec id elit non mi porta.</small>
+                    <h5 class="mb-1">Conveying meaning to assistive technologies Android / ios</h5>
+                    <small>3 дня назад</small>
+                </div>
+                
+                <p class="mb-1">
+                    <i className="bi bi-person"></i> dmk &nbsp;
+                    <i className="bi bi-person-check"></i> predeinay &nbsp;
+                    <i className="bi bi-arrow-right"></i> timofey &nbsp;
+                </p>
+                <small><Badge bg="secondary">Close</Badge></small>
         </ListGroup.Item>
         <ListGroup.Item key={1} 
             action href={"https://ya.ru"}
