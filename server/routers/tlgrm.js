54e7341fd08549acb90214e7b9a11a55dfba08b1
predeinay <predeinay@gmail.com>
2024-01-11 19:37:41 +0500

    bot add task to admin project

--- server/routers/tlgrm.js
+++ server/routers/tlgrm.js
@@ -5,18 +5,38 @@ const crypto = require('crypto');
 const userModel = require('../models/ref_users');
 const fetch = require('node-fetch');
 const config = require('../config');
+var ProjectTask = require('../models/project_task');
 
 router.post('/', async (req, res, next) => {
+    let con;
     try {
-	console.log(req.body);
+	console.log(JSON.stringify(req.body));
 	const telegram_chat_id = req.body.message.chat.id;
-	console.log(telegram_chat_id);
-	const response = await fetch(`${config.telegram_bot_url}sendMessage?chat_id=${telegram_chat_id}&text=${telegram_chat_id}`);
-
+	if (req.body.message.chat.type === "private") {	
+	    const response = await fetch(`${config.telegram_bot_url}sendMessage?chat_id=${telegram_chat_id}&text=${telegram_chat_id}`);
+	} else 
+	if (req.body.message.text.startsWith("@planhelpbot")) {
+	    con = await mysql.getConnection();
+	    const task_title = (req.body.message.text).replace("@planhelpbot ","").substring(0,64);
+	    const task_note = req.body.message.text;
+	    const task_id = await ProjectTask.create(con, {
+                values : {
+                    project_id : 33, // admin project
+		    status_id : 93, // backlog status
+                    task_title,task_note,is_deleted : 'N',
+                    created_on : { expression : "now()" },
+                    created_by : 25  // @planhelpbot 
+                }
+            });
+	    const response = await fetch(`${config.telegram_bot_url}sendMessage?chat_id=${telegram_chat_id}&text=${encodeURI("Ваша заявка зафиксирована в planhelp.ru")}`);
+	}
 	res.send({ok:true});
     } catch(err) {
+	res.send({ok:false});
 	console.log(err);
-    }
+    } finally {
+	con && await mysql.releaseConnection(con);
+    } 
     
 });
 
