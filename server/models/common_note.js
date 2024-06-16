c12a9fe70778f13e7e7131a094f90643436d105d
Анатолий Предеин <predeinay@Anatolijs-MacBook-Pro.local>
2024-06-16 16:35:05 +0500

    add text title in notify for comments

--- server/models/common_note.js
+++ server/models/common_note.js
@@ -130,7 +130,8 @@ static async getRemindNoteList({user_id, entity_tree, limit, offset}, con) {
             task_id : values.task_id
         }}))[0];
 
-        const taskUrl = `https://planhelp.ru/project/${projectTaskModel.project_id}/task/${projectTaskModel.task_id}/`;
+        const taskUrl = `<a href="https://planhelp.ru/project/${projectTaskModel.project_id}/task/${projectTaskModel.task_id}/">№${projectTaskModel.task_id} ${projectTaskModel.task_title}</a>`;
+        // const taskUrl = `https://planhelp.ru/project/${projectTaskModel.project_id}/task/${projectTaskModel.task_id}/`;
         let notifyText = "";
         const notifyUserSet = new Set();
 
